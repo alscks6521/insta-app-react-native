c74bf07dd3d9162abd160eaebad703660299f90f
Minsung <alscks6521@naver.com>
2024-05-23 14:13:24 +0900

    ..

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# insta-app-react-native
+# 10week
+
+1. 9주차에는 권한설정하였고, 이미지 고르기 ImagePicker.launchImageLibraryAsync (profile-container.tsx)
+
+2. **이미지를 BinaryLargeObject로 변환 후 Firebase Storage에 profil폴더에 업로드**
+
+   1. Firebase에 저장할 위치 설정 및 업로드
+
+3. **firebaseConfig.tsx 수정** (Firebase에서 Authentication만 사용하게 해놨었음.)
+
+   1. import { getStorage } from "firebase/storage";
+   2. const storage = getStorage(app);
+
+4. auth.currentUser의 사용자 값을 utils 폴더 파일 만듦
+
+   1. assets => Blob 데이터로 변환하고 반환 하는 코드 구현
+   2. 프로필 기본 이미지 코드 구현
+
+5. setUser의 타입을 MyUser 타입을 따로 생성
--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -2,6 +2,8 @@ import { User } from "firebase/auth";
 import { useState } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import styled from "styled-components";
+import { MyUser } from "../screens/profile/profile-container";
+import { defaultImage } from "../utils/utils";
 
 const Container = styled(View)``;
 
@@ -41,22 +43,21 @@ const ProfileImg = styled(Image)`
 const CustomButton = styled(TouchableOpacity)``;
 
 type Props = {
-  user: User | null;
+  user: MyUser | undefined;
   onEditImage: () => void;
 };
 
 export default ({ user, onEditImage }: Props) => {
-  // const [email, setEmail] = useState(user?.email);
   return (
     <Container>
       <Info>
         <CustomButton onPress={onEditImage}>
-          <ProfileImg source={require("../assets/splash.png")} />
+          <ProfileImg source={defaultImage(user?.photoURL)} />
         </CustomButton>
         <Data>
-          <Name>{user?.displayName}</Name>
+          <Name>{user?.name}</Name>
           <Email>{user?.email}</Email>
-          <JoinDate>{user?.metadata.creationTime}</JoinDate>
+          <JoinDate>{user?.creationTime}</JoinDate>
         </Data>
       </Info>
     </Container>
